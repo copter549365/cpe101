4bbb5a2ba2738d6e58b92bfec82fcbcd123abc41
copter549365 <82475237+copter549365@users.noreply.github.com>
2024-10-29 23:28:13 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,71 @@
+
+  $(function () {
+    'use strict'
+
+    // MENU
+    $('.navbar .nav-link').on('click',function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).on('scroll', function() {     
+                                
+        /*----------------------------------------------------*/
+        /*  Navigtion Menu Scroll
+        /*----------------------------------------------------*/    
+        
+        var b = $(window).scrollTop();
+        
+        if( b > 72 ){       
+            $(".navbar").addClass("scroll");
+        } else {
+            $(".navbar").removeClass("scroll");
+        }               
+    });
+
+    // TESTIMONIALS CAROUSEL
+    $('#testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            900:{
+                items:2,
+            },
+            1200:{
+                items:3,
+                loop:false
+            }
+        }
+    })
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.navbar .nav-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });   
+     
+  });
+
+  let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (index + slides.length) % slides.length; // Loop around
+    slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+// Change slide every 3 seconds
+setInterval(nextSlide, 3000);
